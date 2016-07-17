e653f9be78c79998ea0a573025a73fd63730c54e
Chris Just <chris.just@blueprintsys.com>
2016-07-17 19:41:44 -0400

    Added TestQueue and some unit tests for it.

---
+++ src/NTestController/TestQueue.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using Utilities;
+using Logger;
+
+namespace NTestController
+{
+    /// <summary>
+    /// The result of a single test execution.
+    /// </summary>
+    public class TestResult
+    {
+        /// <summary>Exit result of a test.</summary>
+        public enum ExitResult
+        {
+            NotRun,
+            Pass,
+            Fail,
+            Error,
+            Ignored,
+            Inconclusive
+        }
+
+        /// <summary>The pass/fail result of the test.</summary>
+        public ExitResult Result { get; set; }
+
+        /// <summary>The standard output of the test.</summary>
+        public string Output { get; set; }
+
+        /// <summary>The error message if the test failed, otherwise null.</summary>
+        public string Error { get; set; }
+
+        /// <summary>The stack trace if an error occurred, otherwise null.</summary>
+        public string StackTrace { get; set; }
+
+        /// <summary>If supported by the TestExecutor plugin, this is the location of the output file for this test attempt.</summary>
+        public string OutputFile { get; set; }
+
+        /// <summary>The source computer where this test was executing.</summary>
+        public string ExecutionSource { get; set; }
+
+        /// <summary>The target of this test (ex. the computer or URL where the test was executing against).</summary>
+        public string ExecutionTarget { get; set; }
+
+        /// <summary>The test execution time in milliseconds.</summary>
+        public int ExecutionTime { get; set; }
+
+        /// <summary>The test timeout in milliseconds.  0 indicates no timeout.</summary>
+        public int Timeout { get; set; }
+
+        /// <summary>Plugins can add their own custom properties here, with the key being the property name.</summary>
+        public Dictionary<string, object> ExtendedProperties { get; } = new Dictionary<string, object>();
+    }
+
+    /// <summary>
+    /// A test to run including the full test name and the results of each retry attempt.
+    /// </summary>
+    public class Test
+    {
+        private string _testName = null;
+
+        /// <summary>The fully qualified name of the test (ex. namespace.class.function).</summary>
+        public string TestName
+        {
+            get
+            {
+                if ((_testName == null) && !string.IsNullOrWhiteSpace(TestNamespace) &&
+                    !string.IsNullOrWhiteSpace(TestClass) && !string.IsNullOrWhiteSpace(TestFunction))
+                {
+                    return StringUtils.FormatInvariant("{0}.{1}.{2}",
+                        TestNamespace, TestClass, TestFunction);
+                }
+
+                return _testName;
+            }
+            set
+            {
+                _testName = value;
+            }
+        }
+
+        /// <summary>If supported by the TestExecutor plugin, the namespace of the test.</summary>
+        public string TestNamespace { get; set; }
+
+        /// <summary>If supported by the TestExecutor plugin, the class name (or class.subclasses) of the test.</summary>
+        public string TestClass { get; set; }
+
+        /// <summary>If supported by the TestExecutor plugin, the test function name (and any function parameters used in the test).</summary>
+        public string TestFunction { get; set; }
+
+        /// <summary>A list of test results for each attempt.  The first attempt is at index 0 and the latest result is at the highest index.</summary>
+        public IList<TestResult> TestRuns { get; } = new List<TestResult>();
+
+        /// <summary>Plugins can add their own custom properties here, with the key being the property name.</summary>
+        public Dictionary<string, object> ExtendedProperties { get; } = new Dictionary<string, object>();
+    }
+
+    /// <summary>
+    /// A test queue to hold all of the tests to be run (or that completed).
+    /// </summary>
+    public class TestQueue
+    {
+        #region Member variables
+
+        private readonly object _lock = new object();
+        private Queue<Test> _testsToRun = new Queue<Test>();
+        private List<Test> _completedTests = new List<Test>();
+        private static ILogger Log { get; } = ConsoleLogger.Instance;
+
+        #endregion Member variables
+
+        /// <summary>Gets a collection of completed tests.</summary>
+        public IReadOnlyCollection<Test> CompletedTests
+        {
+            get { return _completedTests.AsReadOnly(); }
+        }
+
+        /// <summary>
+        /// Enqueues a test to run.
+        /// </summary>
+        /// <param name="test">The test to add to the queue.</param>
+        public void EnqueueTestToRun(Test test)
+        {
+            ThrowIf.ArgumentNull(test, nameof(test));
+
+            Log.WriteDebug("Queueing test: {0}", test.TestName);
+
+            lock(_lock)
+            {
+                _testsToRun.Enqueue(test);
+            }
+        }
+
+        /// <summary>
+        /// Dequeues a test to run.
+        /// </summary>
+        /// <returns>A test to run or null if the queue is empty.</returns>
+        public Test DequeueTestToRun()
+        {
+            Test test = null;
+
+            lock (_lock)
+            {
+                if (_testsToRun.Count > 0)
+                {
+                    test = _testsToRun.Dequeue();
+                }
+            }
+
+            return test;
+        }
+
+        /// <summary>
+        /// Adds a test that finished and has no retries left.
+        /// </summary>
+        /// <param name="test">The test to add to the list.</param>
+        public void AddCompletedTest(Test test)
+        {
+            ThrowIf.ArgumentNull(test, nameof(test));
+
+            lock(_lock)
+            {
+                _completedTests.Add(test);
+            }
+        }
+    }
+}
+
---
+++ src/Tests/NTestControllerTests/TestQueueTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using NUnit.Framework;
+using NTestController;
+using Utilities;
+using Logger;
+using System.Collections.Generic;
+
+namespace NTestControllerTests
+{
+    [TestFixture]
+    public class TestQueueTests
+    {
+        private TestQueue _testqueue = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testqueue = new TestQueue();
+        }
+        
+        [TestCase]
+        public void EnqueueTestToRun_NullTest_ArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _testqueue.EnqueueTestToRun(null),
+                "{0}.{1}() should throw an ArgumentNullException if you pass it a null test!",
+                nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+        }
+
+        [Test, Combinatorial]
+        public void EnqueueTestToRun_ValidTest_TestCanBeDequeued(
+            [Values(null, "MyNamespace")]   string testNamespace,
+            [Values(null, "MyClass")]       string testClass,
+            [Values(null, "MyFunction")]    string testFunction,
+            [Values(null, "A.B.C(1,'a')")]  string testName
+        )
+        {
+            // Setup:
+            Test test = new Test {
+                TestNamespace = testNamespace,
+                TestClass = testClass,
+                TestFunction = testFunction,
+                TestName = testName ?? StringUtils.FormatInvariant("{0}.{1}.{2}",
+                    testNamespace, testClass, testFunction)
+            };
+
+            // Execute:
+            Assert.DoesNotThrow(() => _testqueue.EnqueueTestToRun(test),
+                "{0}.{1}() shouldn't throw an Exception if you pass it a valid test!",
+                nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+
+            // Verify:
+            Test dequeuedTest = null;
+
+            Assert.DoesNotThrow(() => dequeuedTest = _testqueue.DequeueTestToRun(),
+                "{0}.{1}() shouldn't throw an exception if a test has been added to the queue!",
+                nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+
+            AssertTestsAreEqual(test, dequeuedTest);
+        }
+
+        [TestCase]
+        public void DequeueTestToRun_NoTests_ReturnsNull()
+        {
+            Test dequeuedTest = null;
+
+            // Execute:
+            Assert.DoesNotThrow(() => dequeuedTest = _testqueue.DequeueTestToRun(),
+                "{0}.{1}() shouldn't throw an exception!",
+                nameof(TestQueue), nameof(TestQueue.DequeueTestToRun));
+
+            // Verify:
+            Assert.IsNull(dequeuedTest,
+                "{0}.{1}() should return null if the queue is empty!",
+                nameof(TestQueue), nameof(TestQueue.DequeueTestToRun));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void DequeueTestToRun_MultipleValidTests_AllTestsCanBeDequeued(int numberOfTests)
+        {
+            // Setup:
+            IList<Test> tests = new List<Test>();
+
+            for (int i = 0; i < numberOfTests; ++i)
+            {
+                Test test = new Test
+                {
+                    TestName = StringUtils.FormatInvariant("Namespace{0}.Class{0}.Function{0}", i)
+                };
+
+                tests.Add(test);
+
+                Assert.DoesNotThrow(() => _testqueue.EnqueueTestToRun(test),
+                    "{0}.{1}() shouldn't throw an Exception if you pass it a valid test!",
+                    nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+            }
+
+            // Execute & Verify:
+            int numberOfTestsDequeued = 0;
+            Test dequeuedTest = null;
+
+            Assert.DoesNotThrow(() => dequeuedTest = _testqueue.DequeueTestToRun(),
+                "{0}.{1}() shouldn't throw an exception if a test has been added to the queue!",
+                nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+            
+            do
+            {
+                if (dequeuedTest != null)
+                {
+                    ++numberOfTestsDequeued;
+
+                    Assert.That(tests.Contains(dequeuedTest),
+                        "Dequeued test '{0}' wasn't found in the list of tests that was enqueued!",
+                        dequeuedTest.TestName);
+
+                    Assert.DoesNotThrow(() => dequeuedTest = _testqueue.DequeueTestToRun(),
+                        "{0}.{1}() shouldn't throw an exception if a test has been added to the queue!",
+                        nameof(TestQueue), nameof(TestQueue.EnqueueTestToRun));
+                }
+            } while (dequeuedTest != null);
+
+            Assert.AreEqual(numberOfTests, numberOfTestsDequeued,
+                "The number of tests Enqueued: {0} doesn't equal the number Dequeued: {1}!",
+                numberOfTests, numberOfTestsDequeued);
+        }
+
+        // TODO: Add tests for TestQueue.AddCompletedTest() and TestQueue.CompletedTests.
+
+
+        /// <summary>
+        /// Asserts the two Tests are equal.
+        /// </summary>
+        /// <param name="test1">First test to compare.</param>
+        /// <param name="test2">Second test to compare.</param>
+        private void AssertTestsAreEqual(Test test1, Test test2)
+        {
+            Assert.AreEqual(test1.TestNamespace, test2.TestNamespace,
+                "TestNamespace of test1 doesn't match test2!");
+            Assert.AreEqual(test1.TestClass, test2.TestClass,
+                "TestClass of test1 doesn't match test2!");
+            Assert.AreEqual(test1.TestFunction, test2.TestFunction,
+                "TestFunction of test1 doesn't match test2!");
+            Assert.AreEqual(test1.TestName, test2.TestName,
+                "TestName of test1 doesn't match test2!");
+            Assert.AreEqual(test1.TestRuns.Count, test2.TestRuns.Count,
+                "TestRuns of test1 has {0} runs, but test2 has {1} runs!",
+                test1.TestRuns.Count, test2.TestRuns.Count);
+            Assert.AreEqual(test1.ExtendedProperties.Count, test2.ExtendedProperties.Count,
+                "ExtendedProperties of test1 has {0} properties, but test2 has {1} properties!",
+                test1.ExtendedProperties.Count, test2.ExtendedProperties.Count);
+        }
+    }
+}
+
