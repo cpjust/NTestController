eae4953b229c78bd4b8a098b06a47a7086b703c6
Chris Just <chris.just@blueprintsys.com>
2016-12-29 01:59:30 -0500

    Added a sample test input file and code to read & parse the input file in the NUnit-Reader plugin.

--- src/Plugins/NUnit-Reader/NUnitReader.cs
+++ src/Plugins/NUnit-Reader/NUnitReader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NTestController;
 using Utilities;
+using System.Collections.Generic;
+using System.IO;
 
 [assembly: CLSCompliant(true)]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
@@ -8,9 +10,20 @@ namespace NUnitReader
 {
     public class NUnitReaderPlugin : IPlugin
     {
-        public NUnitReaderPlugin(string xmlConfig)
+        private string _testInputFile;
+        private List<TestInput> _tests = new List<TestInput>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NUnitReader.NUnitReaderPlugin"/> class.
+        /// </summary>
+        /// <param name="testInputFile">Test input file.</param>
+        public NUnitReaderPlugin(string testInputFile)
         {
-            ThrowIf.StringIsNullOrWhiteSpace(xmlConfig, nameof(xmlConfig));
+            ThrowIf.StringIsNullOrWhiteSpace(testInputFile, nameof(testInputFile));
+
+            _testInputFile = testInputFile;
+
+            ParseTestInputFile(_testInputFile);
         }
 
         #region Inherited from IPlugin
@@ -27,6 +40,44 @@ namespace NUnitReader
         }
 
         #endregion Inherited from IPlugin
+
+        /// <summary>
+        /// Parses the test input file into a list of TestInput objects.
+        /// </summary>
+        /// <param name="testInputFile">Test input file.</param>
+        private void ParseTestInputFile(string testInputFile)
+        {
+            // Read input file and add to _tests.
+            if (!File.Exists(testInputFile))
+            {
+                throw new FileNotFoundException(string.Format("Couldn't find file: {0}", testInputFile));
+            }
+
+            var fileLines = File.ReadAllLines(testInputFile);
+
+            foreach (string fileLine in fileLines)
+            {
+                string line = fileLine.Trim();
+
+                // Skip blank lines or commented out lines.
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                var lineParts = line.Split(new char[] {'|'}, count: 2);
+
+                if (lineParts.Length != 2)
+                {
+                    throw new InvalidDataException(
+                        string.Format("Expected 2 parts ('|' separated) but found {0} parts!", lineParts.Length));
+                }
+
+                var testInput = new TestInput(lineParts[0], lineParts[1]);
+
+                _tests.Add(testInput);
+            }
+        }
     }
 }
 
---
+++ src/Plugins/NUnit-Reader/TestInput.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Utilities;
+
+namespace NUnitReader
+{
+    public class TestInput
+    {
+        private string _testFullName;
+
+        public string DllPath;
+        public string TestFullName
+        {
+            get { return _testFullName; }
+            set
+            {
+                _testFullName = value;
+                ParseTestName(value);
+            }
+        }
+
+        public string TestNamespace;
+        public string TestClass;
+        public string TestFunction;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NUnitReader.TestInput"/> class.
+        /// </summary>
+        /// <param name="dllPath">The path to the DLL.</param>
+        /// <param name="fullTestName">Fully qualified test name
+        ///     (either 'namespace' or 'namespace.class' or 'namespace.class.function'.</param>
+        public TestInput(string dllPath, string fullTestName)
+        {
+            DllPath = dllPath;
+            TestFullName = fullTestName;
+        }
+
+        #region Private functions
+
+        /// <summary>
+        /// Splits the full test name into it's namespace, class and test name components.
+        /// </summary>
+        /// <param name="testName">The full test name.</param>
+        private void ParseTestName(string testName)
+        {
+            ThrowIf.StringIsNullOrWhiteSpace(testName, nameof(testName));
+
+            var nameParts = testName.Split('.');
+
+            TestNamespace = nameParts[0];
+
+            if (nameParts.Length > 1)
+            {
+                // Find the last part that isn't the test function name.
+                // If the test name has parameters, it will contain a parentheses.
+                int testNameIndex;
+
+                for (testNameIndex = 1; testNameIndex < nameParts.Length; ++testNameIndex)
+                {
+                    if (nameParts[testNameIndex].Contains("("))
+                    {
+                        break;
+                    }
+                }
+
+                TestClass = string.Join(".", nameParts, startIndex: 1, count: testNameIndex - 1);
+
+                // Everything else must be the test function name (and any parameters).
+                if (nameParts.Length > 2)
+                {
+                    TestFunction = string.Join("", nameParts, startIndex: testNameIndex, count: nameParts.Length - testNameIndex);
+                }
+            }
+        }
+
+        #endregion Private functions
+    }
+}
+
