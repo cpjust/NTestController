f8b0dba0873bb6287303565915afc65d2149826b
andrewpoch <andrew.pochapsky@blueprintsys.com>
2017-07-17 11:49:18 -0400

    Improved regex formating

--- src/NUnitTestExtractor/NUnitTestExtractorTests/TestExtractorTests.cs
+++ src/NUnitTestExtractor/NUnitTestExtractorTests/TestExtractorTests.cs
@@ -43,15 +43,15 @@ namespace NUnitTestExtractorTests
             switch (myLevel)
             {
                 case NUnitTestExtractorApp.Level.Namespace:
-                    regex = new Regex(@"^(\w+)[\w+\s\|\s\w+$]");
+                    regex = new Regex(@"^(\""(.+)""\s+)\|\s\w+$");
                     break;
 
                 case NUnitTestExtractorApp.Level.Class:
-                    regex = new Regex(@"^(\w+)[\w+\s\|\s\w+\.\w+$]");
+                    regex = new Regex(@"^(\""(.+)""\s+)\|\s\w+\.\w+$");
                     break;
 
                 case NUnitTestExtractorApp.Level.Function:
-                    regex = new Regex(@"^(\w+)[\w+\s\|\s\w+\.\w+\.\w+$]");
+                    regex = new Regex(@"^(\""(.+)""\s+)\|\s\w+\.\w+\.\w+$");
                     break;
             }
             return regex;
